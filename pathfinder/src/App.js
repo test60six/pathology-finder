5137a28b877531c8334b83c766f4b77b9203010c
Dennis <dennis@Test60six-2.local>
2017-12-18 20:46:11 -0500

    form working in react

--- pathfinder/src/App.js
+++ pathfinder/src/App.js
@@ -6,14 +6,49 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="container">
+<div className="jumbotron">PATHFINDER</div>
+<br/>
+<form>
+  <div className="form-group">
+    <label for="thickness">Epidermal Thickness</label>
+    <input type="text" className="form-control" id="thick" placeholder="Score of 1-10"/>
+  </div>
+  <br/>
+  <div className="form-group">
+    <label for="Inflammation">Inflammation</label>
+    <input type="text" className="form-control" id="inflame" placeholder="Score of 1-10"/>
+  </div>
+  <br/>
+  <div className="form-group">
+    <label for="appearance">Microabcesses</label>
+    <input type="text" className="form-control" id="abcess" placeholder="Number of abcesses noted"/>
+  </div>
+  <br/>
+  <div className="form-group">
+    <label for="Stratum Corneum thinning"> Stratum Corneum Thinning</label>
+    <input type="text" className="form-control" id="Corneum" placeholder="Score of 1-10"/>
+  </div>
+  <br/>
+  <div className="form-group">
+    <label for="Parkakreratosis">Parkakreratosis</label>
+    <input type="text" className="form-control" id="parker" placeholder="Score of 1-10"/>
+  </div>
+  <br/>
+  <div className="form-group">
+    <label for="pappy">Papillary Papillae Congestion</label>
+    <input type="text" className="form-control" id="pappy" placeholder="Score of 1-10"/>
+  </div>
+  <br/>
+  <div className="form-group">
+    <label for="Length">Length of Rete Ridges</label>
+    <input type="text" className="form-control" id="length" placeholder="mm"/>
+  </div>
+<button type="submit" className="btn btn-default">Submit</button>
+</form>
+</div>
+</div>
+
     );
   }
 }
