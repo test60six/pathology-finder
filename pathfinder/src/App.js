9dd9c1c38a8c9d53fbd433bceda4903dab495800
Dennis <dennis@Test60six-2.local>
2017-12-23 16:30:56 -0500

    upadte

--- pathfinder/src/App.js
+++ pathfinder/src/App.js
@@ -1,56 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Formies from './Formies.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-<div className="jumbotron"> <img src="./images/logo_pathfinder2.png" alt="logo" width="500" height="75"/> </div>
-<br/>
-<form>
-  <div className="form-group">
-    <label for="thickness">Epidermal Thickness</label>
-    <input type="text" className="form-control" id="thick" placeholder="Score of 1-10"/>
-  </div>
-  <br/>
-  <div className="form-group">
-    <label for="Inflammation">Inflammation</label>
-    <input type="text" className="form-control" id="inflame" placeholder="Score of 1-10"/>
-  </div>
-  <br/>
-  <div className="form-group">
-    <label for="appearance">Microabcesses</label>
-    <input type="text" className="form-control" id="abcess" placeholder="Number of abcesses noted"/>
-  </div>
-  <br/>
-  <div className="form-group">
-    <label for="Stratum Corneum thinning"> Stratum Corneum Thinning</label>
-    <input type="text" className="form-control" id="Corneum" placeholder="Score of 1-10"/>
-  </div>
-  <br/>
-  <div className="form-group">
-    <label for="Parkakreratosis">Parkakreratosis</label>
-    <input type="text" className="form-control" id="parker" placeholder="Score of 1-10"/>
-  </div>
-  <br/>
-  <div className="form-group">
-    <label for="pappy">Papillary Papillae Congestion</label>
-    <input type="text" className="form-control" id="pappy" placeholder="Score of 1-10"/>
-  </div>
-  <br/>
-  <div className="form-group">
-    <label for="Length">Length of Rete Ridges</label>
-    <input type="text" className="form-control" id="length" placeholder="mm"/>
-  </div>
-<button type="submit" className="btn btn-default">Submit</button>
-</form>
-</div>
-</div>
 
-    );
+  //put react routes for Components/pages here//
+
+  render() {
+
+    return(
+      <div id="mainbody">
+          <Formies/>
+     </div>
+s
+);
+
+
+
+
   }
 }
+
+
 
 export default App;
---
+++ pathfinder/src/Results.js
@@ -0,0 +1,2 @@
+//  this is the high charts page
+import React, { Component } from 'react';s
