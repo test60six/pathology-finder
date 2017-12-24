20b15f1934a4f94420d1d260235e2e2f12242908
Dennis <dennis@test60six.home>
2017-12-24 18:47:03 -0500

    updated background and created results page

--- pathfinder/src/Results.js
+++ pathfinder/src/Results.js
@@ -1,2 +1,34 @@
 //  this is the high charts page
-import React, { Component } from 'react';s
+import React from 'react';
+import './App.css';
+import './index.css';
+
+function Results (props){
+  return(
+    <div>HIGH CHARTS RESULTS SHOULD DUMP HERE, RENDER THROUGH App.js div"mainbody"
+
+    </div>
+  )};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Results;
